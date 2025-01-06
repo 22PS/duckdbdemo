a50e594b13d7c961e485440b83d76d18c4732a10
Pulkit Singhal <149754587+22PS@users.noreply.github.com>
2025-01-06 16:57:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const queryRoutes = require('./routes/queryRoutes');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://duckdbdemo.vercel.app"],
+    methods:["POST", "GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 app.use('/api', queryRoutes);
